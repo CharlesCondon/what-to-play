7e2ccc29a34dc9e0021771b0830402b931a23d31
Charles <charlescon@live.com>
2025-11-10 18:23:41 -0600

    added animations to background component

--- components/BackgroundPattern/BackgroundPattern.tsx
+++ components/BackgroundPattern/BackgroundPattern.tsx
@@ -1,16 +1,16 @@
 export default function BackgroundPattern() {
     return (
-        <div className="fixed inset-0 -z-10 overflow-hidden bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
+        <div className="fixed inset-0 -z-10 overflow-hidden animate-hue bg-linear-to-br from-slate-900 via-purple-900 to-slate-900">
             <div className="fixed inset-0 opacity-10">
                 <div
-                    className="absolute inset-0"
+                    className="absolute inset-0 animate-grid-move"
                     style={{
                         backgroundImage: `linear-gradient(#00ffff 1px, transparent 1px), linear-gradient(90deg, #00ffff 1px, transparent 1px)`,
                         backgroundSize: "50px 50px",
                         transform: "perspective(500px) rotateX(80deg)",
                         transformOrigin: "center center",
                     }}
-                ></div>
+                />
             </div>
         </div>
     );
